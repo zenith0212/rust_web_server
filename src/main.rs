fbc8807608605634a248bdaf39622142e756d223
zenith1995 <versatile.skydev@gmail.com>
2023-03-19 15:38:48 -0400

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -16,22 +16,21 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    // let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK\r\n\r\n";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
-    
-        let response = format!("{status_line}\r\n \r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
-    
-        // println!("request: {:#?}", http_request);
-        println!("response: {}", response);
-    }
-    else {
-        println!("{}", request_line);
-    }
 
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\n\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
